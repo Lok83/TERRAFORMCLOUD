2aa2be7bd7cab9f6f19ac2769db24ce294fe4119
Lok83 <96905329+Lok83@users.noreply.github.com>
2022-06-08 11:49:58 +0530

    Add files via upload

---
+++ var.tf
@@ -0,0 +1,34 @@
+variable "region" {
+  default = "us-east-1"
+}
+variable "secgroupid" {
+  default = "sg-011f1f2cc3cf61f1d"
+}
+variable "description" {
+  default = "camefromvarfile ss"
+}
+
+variable "tag" { default = "tagterraform" }
+
+variable "ami_name_value" {
+  default = "amzn-ami-vpc-nat*"
+}
+
+variable "ami_virtualization_type" {
+  default = "hvm"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "iscreate" {
+  default = "true"
+}
+
+variable "sg_id" {
+  default = ""
+}
+
+variable "keyname" { default = "" }
+variable "vpc_id" { default = "" }
